da1dd137f85dda30626e5141564ded25b3312613
Aemjirapat.tr <aemjirapat.tr@gmail.com>
2026-03-11 15:47:04 +0700

    feat: add jsdoc

--- libs/core/src/application/reports/build-report.use-case.ts
+++ libs/core/src/application/reports/build-report.use-case.ts
@@ -7,6 +7,16 @@ import { Response } from 'express';
 export class BuildReportUseCase {
   constructor(private xlsxProvider: XLSXProvider) {}
 
+  /**
+   * Build and stream an XLSX file to the HTTP response.
+   *
+   * This method delegates the file generation to `xlsxProvider.buildFile`
+   * using the configuration provided in the `IBuildXLSX` DTO.
+   *
+   * @param {IBuildXLSX} dto - Data transfer object containing file options and table data.
+   * @param {Response} response - Express response object used to send the generated XLSX file to the client.
+   * @returns {Promise<void>} Resolves when the XLSX file has been generated and written to the response stream.
+   */
   async buildXLSX(dto: IBuildXLSX, response: Response) {
     return await this.xlsxProvider.buildFile(
       dto.option.topic,
